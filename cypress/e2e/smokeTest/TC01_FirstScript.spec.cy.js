8068034d5a9e5236495766775a91b9abfba1e2ee
Aubrey Fredericks <AubreyFredericks@woolworths.co.za>
2024-04-21 21:01:54 +0200

    pushed final changes

--- cypress/e2e/smokeTest/TC01_FirstScript.spec.cy.js
+++ cypress/e2e/smokeTest/TC01_FirstScript.spec.cy.js
@@ -6,7 +6,7 @@ Cypress.config('baseUrl','https://react-redux.realworld.io/')
 describe('Login Functionality', function(){
 
     it('Login Test using Conduit', function(){
-        cy.visit('/')
+        cy.visit('https://react-redux.realworld.io/')
         cy.get('a[href="#login"]').click()
         cy.get('input[type="email"]').type('cypress@automation.co.za')
         cy.get('input[type="password"]').type('test1234Cypress')
